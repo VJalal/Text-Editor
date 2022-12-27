3286f1e016bd924cc2cae472b34caaac7bde5a38
VJalal <vjalalvandi@gmail.com>
2022-12-27 15:26:09 -0500

    Added scrolling line numbers.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5 import uic
 
 
@@ -9,9 +10,22 @@ class MyGUI(QMainWindow):
         super(MyGUI, self).__init__()
         uic.loadUi('untitled.ui', self)
         self.show()
+        self.TextBox.textChanged.connect(self.textChange)
+        self.Numbers.setReadOnly(True)
+        self.TextBox.verticalScrollBar().valueChanged.connect(
+            self.Numbers.verticalScrollBar().setValue)
+
+    def textChange(self):
+        self.lineNum()
+
+    def lineNum(self):
+        self.Numbers.clear()
+        self.Numbers.setAlignment(Qt.AlignRight)
+        for i in range(1, self.TextBox.toPlainText().count('\n') + 2):
+
+            self.Numbers.insertPlainText(str(i)+'\n')
 
 
 app = QApplication([])
 window = MyGUI()
 app.exec_()
-
